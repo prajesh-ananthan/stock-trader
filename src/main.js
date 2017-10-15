810c6c41ce3f27385209bfbda39da6c848419afd
Prajesh Ananthan <prajesh.ananthan@airasiabig.com>
2017-10-16 02:56:35 +0800

    Added Vuex

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 import VueRouter from 'vue-router';
 import {routes} from './routes';
 
+import store from './store/store';
+
 Vue.use(VueRouter);
 
 const router = new VueRouter({
@@ -14,5 +16,6 @@ const router = new VueRouter({
 new Vue({
     el: '#app',
     router,
+    store,
     render: h => h(App)
 })
---
+++ src/store/modules/stocks.js
@@ -0,0 +1,43 @@
+import stocks from '../../data/stocks';
+
+const state = {
+    stocks: []
+};
+
+const mutations = {
+    'SET_STOCKS'(state, stocks) {
+        state.stocks = stocks;
+    },
+    'RND_STOCKS'(state) {
+
+    }
+};
+
+// Setters
+const actions = {
+    buyStock: ({commit}, order) => {
+        commit();
+    },
+    initStocks: ({commit}) => {
+        commit('SET_STOCKS', stocks)
+    },
+    randomizeStocks: ({commit}) => {
+        commit('RND_STOCKS');
+    }
+};
+
+// Getters
+const getters = {
+    stocks: state => {
+        return state.stocks;
+    }
+};
+
+// export in one object
+export default {
+    state,
+    mutations,
+    actions,
+    getters
+}
+
